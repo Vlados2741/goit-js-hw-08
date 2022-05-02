51a130bd1fe14e8fb2f2b9d85df4a81ca98aaf70
Vlados2741 <ramosinho3@gmail.com>
2022-05-02 22:53:47 +0300

    done task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
+import Player from '@vimeo/player';
+import _ from 'lodash';
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+player.on('play', function() {
+    console.log('played the video!');
+});
 
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
+
+const onPlay = function(data) {
+    player.getCurrentTime().then(function (seconds) {
+        localStorage.setItem(STORAGE_KEY, seconds);
+    });
+};
+
+player.on('play', onPlay);
+
+const updateTime = function () {
+    const time = localStorage.getItem(STORAGE_KEY);
+    player.setCurrentTime(time).then(function (seconds) {
+        console.log(seconds)
+    }).catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                break;
+
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+};
+player.on('play', updateTime);
+
+player.on('play', _.throttle(updateTime, 1000));
