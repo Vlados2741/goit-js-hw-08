c1336937f7a946835991dd78ba5257797654a419
Vlados2741 <ramosinho3@gmail.com>
2022-05-10 15:33:19 +0300

    DONE

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+const throttle = require('lodash.throttle');
+const refs = {
+    form: document.querySelector(`.feedback-form`),
+    button: document.querySelector(`button`),
+    input: document.querySelector(`input`),
+    textarea: document.querySelector(`textarea`),
+};
+ 
+const onFormInput = (e) => {
+    const {
+        elements: { email, message },
+    } = e.currentTarget;
+    const user = {
+        email: email.value,
+        message: message.value
+    }
+    localStorage.setItem("feedback-form-state", JSON.stringify(user));
+};
+
+function fillForm() {
+    const savedData = localStorage.getItem("feedback-form-state");
+    if (savedData) {
+        refs.input.value = JSON.parse(savedData).email;
+        refs.textarea.value = JSON.parse(savedData).message;
+    }
+};
+
+fillForm()
+
+const onSubmitForm = (e) => {
+    e.preventDefault();
+    const {
+        elements: { email, message },
+    } = e.currentTarget;
+    const user = {
+        email: email.value,
+        message: message.value
+    }
+    console.log(user);
+    e.target.reset();
+    localStorage.removeItem("feedback-form-state");
+    
+};
+refs.form.addEventListener(`input`, throttle(onFormInput, 500));
+refs.form.addEventListener(`submit`, onSubmitForm);
 
