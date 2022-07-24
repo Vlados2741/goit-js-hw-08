b98c5dc43cb148a672104f00387c4e450dc80fa6
Vlados2741 <ramosinho3@gmail.com>
2022-07-24 11:46:25 +0300

    done 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,20 +8,7 @@ const noteTime = function (data) {
     localStorage.setItem("videoplayer-current-time", data.seconds);
     console.log(data.seconds)
 };
-// player.on('timeupdate', noteTime);
 
-const updateTime = function () {
-    const time = localStorage.getItem("videoplayer-current-time");
-    player.setCurrentTime(time).then(function (seconds) {
-        // console.log(seconds)
-    }).catch(function (error) {
-        switch (error.name) {
-            case 'RangeError':
-                break;
-            default:
-                break;
-        }
-    });
-};
 player.on('timeupdate', _.throttle(noteTime, 1000));
-player.on('play', updateTime);
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
